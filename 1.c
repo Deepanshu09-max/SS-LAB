a37f4a4512768dd953bff756b829a4a883b34516
Deepanshu-max <deepanshusaini2212@gmail.com>
2024-08-29 20:04:56 +0530

    1.a

--- 1.c
+++ 1.c
@@ -1,3 +1,13 @@
+// Deepanshu Saini
+// MT2024039
+
+/*
+1. Create the following types of a files using (i) shell command (ii) system call
+a. soft link (symlink system call)
+b. hard link (link system call)
+c. FIFO (mkfifo Library Function or mknod system call)
+*/
+
 #include <stdio.h>
 #include <unistd.h>
 
