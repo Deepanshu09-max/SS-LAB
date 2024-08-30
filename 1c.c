2e206fd9138c7dc458675cc9e698a7c79a7fa54a
Deepanshu-max <deepanshusaini2212@gmail.com>
2024-08-30 10:50:39 +0530

    2.c

---
+++ 1c.c
@@ -0,0 +1,23 @@
+// Deepanshu Saini
+// MT2024039
+
+/*
+1. Create the following types of a files using (i) shell command (ii) system call
+a. soft link (symlink system call)
+b. hard link (link system call)
+c. FIFO (mkfifo Library Function or mknod system call)
+*/
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+
+int main() {
+    int status = mknod("file", S_IFIFO | 0666, 0);
+    if (status == 0) {
+        printf("FIFO created successfully.\n");
+    } else {
+        perror("mknod");
+    }
+    return 0;
+}
