3183913f1b1dbd1386fde74947223d6eea733a5a
Deepanshu-max <deepanshusaini2212@gmail.com>
2024-09-12 16:22:57 +0530

    27b

--- 27d.c
+++ 27d.c
@@ -19,27 +19,6 @@ int main()
 
 /*
 /*
-Parv Ketanbhai Gatecha
-MT2024108
-
-/*
-write a program to execute ls -Rl by the following system calls
-d. execvp
-*/
-
-#include <stdio.h>
-#include <unistd.h>
-int main()
-{
-    // it will use environment variable PATH to run executable PATH .
-    // it does not required entire file path as we did in execl().
-    char *path = "ls";
-    char *argv[] = {path, "-Rl", NULL};
-    execvp(path, argv);
-    return 0;
-}
-
-/*
 .:
 total 204
 -rwxrwxr-x 1 deepanshu-saini deepanshu-saini 15952 Aug 29 19:52 1
